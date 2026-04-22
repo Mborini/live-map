dfb07f003bfabe8ff48a7a63a3694c930a4557d8
mbirni <mborini98@gmail.com>
2026-04-22 20:37:51 +0300

    next auth

--- middleware.ts
+++ middleware.ts
@@ -3,23 +3,27 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET, // ✅ مهم
+  });
 
   const isLoginPage = req.nextUrl.pathname === "/login";
   const isAuth = !!token;
 
-  // ❌ إذا مش مسجل دخول
+  // ❌ غير مسجل
   if (!isAuth && !isLoginPage) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
-  // ❌ إذا مسجل دخول ودخل login
+  // ❌ مسجل وحاول يدخل login
   if (isAuth && isLoginPage) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 
   return NextResponse.next();
 }
+
 export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico).*)",
